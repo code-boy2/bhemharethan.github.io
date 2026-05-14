419af352e66456c350e92cb81735d353b99681e4
alpha-code02 <217216901+code-boy2@users.noreply.github.com>
2026-05-14 12:53:22 +0400

    add

---
+++ pages/javascript.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll(".anime-img");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+images.forEach((img) => {
+    observer.observe(img);
+});
